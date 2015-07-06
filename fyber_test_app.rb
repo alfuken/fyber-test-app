0bdd60a729abc8ad18c11f52de3ed1be84bfe228
Bohdan Shchepanskyy <alfuken@gmail.com>
2015-07-06 18:47:30 +0200

    add endpoints and views

--- fyber_test_app.rb
+++ fyber_test_app.rb
@@ -1,4 +1,13 @@
 require 'sinatra/base'
+require 'rest-client'
 
 class FyberTestApp < Sinatra::Base
+  get '/' do
+    erb :form
+  end
+
+  post '/' do
+    @data = {}
+    erb :results
+  end
 end
